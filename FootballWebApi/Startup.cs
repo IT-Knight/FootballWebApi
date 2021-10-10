36aab7054e50d76fd9d1b9d34921a69bb1f6592d
Nikita Driuma <Nichita.Driuma@endava.com>
2021-10-10 16:42:35 +0300

    Startup.cs typo fix

--- FootballWebApi/Startup.cs
+++ FootballWebApi/Startup.cs
@@ -57,7 +57,7 @@ namespace FootballWebApi
                 //SwaggerConfig.UseAcceptHeaderApiVersion("v");
 
         });  
-            x
+            
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                .AddJwtBearer(options =>
